fe7f13e341933a1e6627922e1477b984a673f791
Yann Renard <76118368+yannouuuu@users.noreply.github.com>
2025-11-28 14:19:26 +0100

    readd jeu.java

--- jeu.java
+++ jeu.java
@@ -1,45 +1,45 @@
-
 // Anaëlle Morey Yann Renard
 
-import extensions.File;
 import extensions.CSVFile;
- 
-class Jeu extends Program {
+import extensions.File;
 
-    void jouer(){
+class jeu extends Program {
 
+    void jouer() {
         println("vouler vous jouer ? :");
         println(" oui = y");
         println(" non = n ");
         char repJ = readChar();
 
-        while(repJ != 'y'){ // si le joueur a une autre reponse que oui(y)
+        while (repJ != 'y') {
+            // si le joueur a une autre reponse que oui(y)
             println("vouler vous jouer ? :");
             println(" oui = y");
             println(" non = n ");
             repJ = readChar();
-
         }
         intro(); //lancer l'introduction
         //clearScreen();
         affichage(); // affichage de l'interface
     }
 
-    void intro(){
+    void intro() {
         // et je pense juste le mettre dans le code de jouer, ca va etre plus simple
         // a mettre ds une bdd je pense mais oklm"
         println("");
-        println("Bienvenue a toi petit entrepreneur, que dirais tu de créer un empire de cookies ? ");
-        println("voici quelque choses a savoir : ");  // explication rapide du fonctionnement du jeu
+        println(
+            "Bienvenue a toi petit entrepreneur, que dirais tu de créer un empire de cookies ? "
+        );
+        println("voici quelque choses a savoir : "); // explication rapide du fonctionnement du jeu
     }
 
-    void affichage(){
-        // idée : nv terminal pour que ce soit clean 
+    void affichage() {
+        // idée : nv terminal pour que ce soit clean
         println("");
         print("Argent : " /*+ argent*/);
         print("Gain de la journée :" /*+ gains*/);
         print(" Date : " /* + date */);
-      /*   if( estTaxé == true){
+        /*   if( estTaxé == true){
             print( "Taxes :" + taxes);
         }
         if( aDesEmployer == true){
@@ -48,17 +48,13 @@ class Jeu extends Program {
         if( coutMatierePremier == true){
             print(" Coût des matières première : " /* + coutMatPrem );
         } */
-       println();
+        println();
     }
 
-    void question(){
-        
-    }
+    void question() {}
 
-    void algorithm(){
-
+    void algorithm() {
         //clearScreen();
         jouer();
-
     }
 }
